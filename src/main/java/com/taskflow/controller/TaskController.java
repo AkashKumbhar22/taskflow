96cab0c58cd40c4bfd103e2b6ce9f79899677e05
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-09 19:26:45 +0530

    Api

--- src/main/java/com/taskflow/controller/TaskController.java
+++ src/main/java/com/taskflow/controller/TaskController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -14,14 +17,60 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
+    // CREATE: POST /tasks
     @PostMapping
-    public ResponseEntity<Task> createtask(@RequestBody Task task){
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
         Task savedTask = taskRepository.save(task);
         return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
     }
+
+    // READ ALL: GET /tasks
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskRepository.findAll();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
+    // READ ONE: GET /tasks/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        
+        if (task.isPresent()) {
+            return new ResponseEntity<>(task.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
+    // UPDATE: PUT /tasks/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        
+        if (taskOptional.isPresent()) {
+            Task task = taskOptional.get();
+            task.setName(taskDetails.getName());
+            task.setStatus(taskDetails.getStatus());
+            task.setPriority(taskDetails.getPriority());
+            
+            Task updatedTask = taskRepository.save(task);
+            return new ResponseEntity<>(updatedTask, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-    
-
+    // DELETE: DELETE /tasks/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        Optional<Task> task = taskRepository.findById(id);
+        
+        if (task.isPresent()) {
+            taskRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}

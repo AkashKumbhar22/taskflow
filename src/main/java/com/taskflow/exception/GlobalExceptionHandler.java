faa7000363da472cfae0dd72f4c55e9c7bb974bc
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-02-10 23:38:15 +0530

    result

--- src/main/java/com/taskflow/exception/GlobalExceptionHandler.java
+++ src/main/java/com/taskflow/exception/GlobalExceptionHandler.java
@@ -1,43 +1,5 @@
-// package com.taskflow.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-
-// import java.time.LocalDateTime;
-// import java.util.HashMap;
-// import java.util.Map;
-
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
-
-//     @ExceptionHandler(ResourceNotFoundException.class)
-//     public ResponseEntity<Map<String, Object>> handleResourceNotFound(ResourceNotFoundException ex) {
-//         Map<String, Object> body = new HashMap<>();
-//         body.put("timestamp", LocalDateTime.now());
-//         body.put("message", ex.getMessage());
-//         body.put("status", HttpStatus.NOT_FOUND.value());
-        
-//         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-//     }
-
-//     @ExceptionHandler(Exception.class)
-//     public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex) {
-//         Map<String, Object> body = new HashMap<>();
-//         body.put("timestamp", LocalDateTime.now());
-//         body.put("message", "Internal server error");
-//         body.put("details", ex.getMessage());
-//         body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        
-//         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
-//     }
-// }
-
-
-
-
-package com.taskflow.exception;
+packaSge com.taskflow.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

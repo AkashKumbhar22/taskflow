52539f289a0dc6dc6c30b47b6a4560dc6c0a2a45
Akash Kumbhar <akashkumbhar2912@gmail.com>
2026-03-19 11:08:43 +0530

    Changes in HealthController

--- src/main/java/com/taskflow/controller/HealthController.java
+++ src/main/java/com/taskflow/controller/HealthController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Taskflow Backend Running ";
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
     }
 }
